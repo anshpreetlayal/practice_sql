5f019f91cedad54f368bc54ea9068bd23f53874a
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-09-29 17:22:21 -0400

    Tables created

--- dummy_data/database.sql
+++ dummy_data/database.sql
@@ -8,11 +8,21 @@ DateJoined int NOT NULL,
 )
 
 CREATE Table Order(
-
+OrderID int NOT NULL PRIMARY KEY,
+CustomerID  int NOT NULL FOREIGN KEY,
+OrderDate int NOT NULL,
+TotalAmount int NOT NULL,
 )
 
 CREATE Table OrderItem(
+OrderItem-ID int NOT NULL PRIMARY KEY,
+OrderID int NOT NULL FOREIGN KEY,
+ProductID int NOT NULL FOREIGN KEY,
+Quantity int NOT NULL,
 )
 
 CREATE Table Product(
+ProductID int NOT NULL PRIMARY KEY,
+ProductName Varchar(20)
+Price int NOT NULL,
 )
