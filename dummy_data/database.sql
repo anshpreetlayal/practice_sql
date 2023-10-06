fa185899d3eae582b904591b5d78cfe17f8ff9d6
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-10-06 16:50:02 -0400

    Inserting values

--- dummy_data/database.sql
+++ dummy_data/database.sql
@@ -31,4 +31,8 @@ Price int NOT NULL,
 Insert into Customer (CustomerID, FirstName, LastName, Email, Phone, DateJoined)
 values (1, 'John', 'Doe', 'john@example.com', 123-345-334, 2021-03-14),
        (2, 'Jane', 'Smith', 'jane@example.com', 122-456-754, 2021-04-12),
-       (20, 'Alice', 'Johnson', 'alice@example.com', );
+       (20, 'Alice', 'Johnson', 'alice@example.com', 456-567-347, 2020-09-19, );
+       (3, 'Katy', 'John', 'katy@example.com', 346-574-357, 2020-03-12, );
+
+
+
