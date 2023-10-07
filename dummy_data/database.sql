ef5322d76f42ad83c5b3665f27310a4c03882645
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-10-07 17:31:54 -0400

    Inserting values

--- dummy_data/database.sql
+++ dummy_data/database.sql
@@ -33,6 +33,18 @@ values (1, 'John', 'Doe', 'john@example.com', 123-345-334, 2021-03-14),
        (2, 'Jane', 'Smith', 'jane@example.com', 122-456-754, 2021-04-12),
        (20, 'Alice', 'Johnson', 'alice@example.com', 456-567-347, 2020-09-19, );
        (3, 'Katy', 'John', 'katy@example.com', 346-574-357, 2020-03-12, );
+        (4, 'Michael', 'Smith', 'michael@example.com', 555-123-4567, '2021-05-20')
+        (5, 'Emily', 'Johnson', 'emily@example.com', 987-654-3210, '2019-11-08')
+        (6, 'David', 'Brown', 'david@example.com', 123-456-7890, '2022-02-15')
+        (7, 'Sophia', 'Williams', 'sophia@example.com', 111-222-3333, '2020-08-04')
+        (8, 'James', 'Davis', 'james@example.com', 444-555-6666, '2021-12-01')
+        (9, 'Olivia', 'Miller', 'olivia@example.com', 777-888-9999, '2018-06-17')
+        (10, 'Ethan', 'Wilson', 'ethan@example.com', 666-999-1111, '2019-03-25')
+        (11, 'Ava', 'Moore', 'ava@example.com', 333-777-2222, '2022-09-10')
+        (12, 'William', 'Lee', 'william@example.com', 123-789-4567, '2020-07-29')
+        (13, 'Emma', 'Anderson', 'emma@example.com', 888-555-4444, '2017-04-12')
+
+
 
 
 
