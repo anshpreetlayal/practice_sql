26a7370825350ce416ca9b8eb405ed8ebeb5f8c2
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-10-10 17:21:23 -0400

    practicing select queries

--- queries/basic/01_select_all.sql
+++ queries/basic/01_select_all.sql
@@ -1,5 +1,31 @@
 -- I will be using this website to practice - https://sqliteonline.com/
 SELECT * from demo;
-
+-- Retries customer id from the customer table
 SELECT CustomerID from Customer;
+
+-- Retrieve all customer data from the Customer table.
+Select * from Customer;
+
+-- Get the first and last names of customers who joined on a specific DateJoined.
+-- Fetch the email and phone number of a customer with a specific CustomerID.
+--List all orders from the Order table.
+-- Retrieve the total amount of a specific order using its OrderID.
+-- Get the OrderDate of a particular order.
+--  List all order items from the OrderItem table.
+--  Retrieve the quantity of a specific product in an order item.
+-- List all products from the Product table.
+-- Get the name and price of a specific product.
+--Retrieve all customer data from the Customer table.
+--Get the first and last names of customers who joined on a specific DateJoined.
+--Fetch the email and phone number of a customer with a specific CustomerID.
+--  List all orders from the Order table.
+ -- Retrieve the total amount of a specific order using its OrderID.
+--  Get the OrderDate of a particular order.
+--  List all order items from the OrderItem table.
+--  Retrieve the quantity of a specific product in an order item.
+--  List all products from the Product table.
+--  Get the name and price of a specific product.
+
+
+
 
