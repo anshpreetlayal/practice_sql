fa8ca78b37527e43741883602e5e5a30b7268edb
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-10-16 19:10:23 -0400

    practicing select queries

--- dummy_data/database.sql
+++ dummy_data/database.sql
@@ -77,6 +77,8 @@ VALUES ( 1, 1 , 101, 3);
        ( 2, 2, 102, 6);
        (3, 3, 103, 4);
        ( 4, 4, 104, 3);
+       ( 5, 5, 104, 3);
+
         
 INSERT INTO Product (ProductID, ProductName, Price)
 VALUES (101, 'Widget A', 20),
