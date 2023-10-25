913e5b5ecb4ce025e427c920cdd918c03386d38a
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-10-24 20:52:08 -0400

    practicing select queries

--- queries/basic/02_select_columns.sql
+++ queries/basic/02_select_columns.sql
@@ -32,4 +32,8 @@
 
 --Select the order IDs and customer IDs from the "Order" table.
 
+
 --Select the order dates and total order amounts from the "Order" table.
+
+
+--Select the product IDs from the "OrderItem" table.
