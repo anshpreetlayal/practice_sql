6a3f0122823980c0a1b30fd3fd42deab80ba4403
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-10-29 21:12:15 -0400

    practicing select queries

--- queries/basic/03_where_clause.sql
+++ queries/basic/03_where_clause.sql
@@ -4,3 +4,12 @@
 
 --What products were ordered by customer Sophia Williams?
 
+--Retrieve all orders with a total amount greater than $600.
+
+--List the products with a price lower than $25.
+
+--Find the email address of the customer who made the order with the highest total amount.
+
+--Which customers placed orders in January 2023?
+
+-
