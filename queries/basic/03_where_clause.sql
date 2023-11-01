e614a33008ebf9ee1e45ee1cc3e7cfd10aa8ef49
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-01 19:48:16 -0400

    practicing select queries

--- queries/basic/03_where_clause.sql
+++ queries/basic/03_where_clause.sql
@@ -35,6 +35,7 @@ WHERE o.TotalAmount = (SELECT MAX(TotalAmount) FROM "Order");
 
 --Which customers placed orders in January 2023?
 SELECT DISTINCT c.FirstName, c.LastName
+FROM Customer c
 
 --Retrieve the order date and total amount for orders placed by customers who joined in 2021.
 
