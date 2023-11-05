e9491ca4b59c2472b774a4e7438306c79fdf0f6d
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-05 12:24:44 -0500

    practicing select queries

--- queries/basic/03_where_clause.sql
+++ queries/basic/03_where_clause.sql
@@ -14,10 +14,6 @@
 
 
 --Which customers placed orders in January 2023?
-SELECT DISTINCT c.FirstName, c.LastName
-FROM Customer c
-JOIN "Order" o ON c.CustomerID = o.CustomerID
-WHERE o.OrderDate BETWEEN '2023-01-01' AND '2023-01-31';
 
 --Retrieve the order date and total amount for orders placed by customers who joined in 2021.
 
