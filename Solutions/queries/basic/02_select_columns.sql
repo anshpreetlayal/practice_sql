1ede403601ceea96969fc126900de5e4833e7a6f
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-05 12:23:01 -0500

    practicing select queries

---
+++ Solutions/queries/basic/02_select_columns.sql
@@ -0,0 +1,67 @@
+
+--Select the first and last names of all customers.
+SELECT FirstName, LastName
+FROM Customer;
+
+--Select the order dates from the "Order" table.
+SELECT OrderDate
+FROM Order;
+
+
+--Select the product names from the "Product" table.
+SELECT ProductName
+FROM Product;
+
+
+--Select the email addresses of customers who joined in 2021.
+SELECT Email
+FROM Customer
+WHERE DateJoined LIKE '2021';
+
+
+--Select the total order amounts from the "Order" table.
+SELECT TotalAmount
+FROM Order;
+
+
+--Select the quantity of products in the "OrderItem" table.
+SELECT Quantity
+FROM OrderItem;
+
+
+--Select the first name and phone number of customers.
+SELECT FirstName, Phone
+FROM Customer;
+
+
+--Select the total order amounts for orders with an amount greater than 700.
+SELECT TotalAmount
+FROM Order
+WHERE TotalAmount > 700;
+
+
+--Select the product names and prices from the "Product" table.
+
+
+--Select the last names of customers who joined before 2020.
+
+
+--Select the order IDs and customer IDs from the "Order" table.
+
+
+--Select the order dates and total order amounts from the "Order" table.
+
+
+--Select the product IDs from the "OrderItem" table.
+
+
+--Select the first names of customers who joined in 2022.
+
+
+--Select the product names and quantities from the "Product" and "OrderItem" tables.
+SELECT p.ProductName, oi.Quantity
+FROM Product p
+JOIN OrderItem oi ON p.ProductID = oi.ProductID;
+
+
+
---
+++ Solutions/queries/basic/03_where_clause.sql
@@ -0,0 +1,49 @@
+--Find the total amount of orders placed by customer John Doe.
+SELECT SUM(o.TotalAmount)
+FROM Order o
+JOIN Customer c ON o.CustomerID = c.CustomerID
+WHERE c.FirstName = 'John' AND c.LastName = 'Doe';
+
+--Which customers joined before September 2020?
+SELECT FirstName, LastName
+FROM Customer
+WHERE DateJoined < '2020-09-01';
+
+--What products were ordered by customer Sophia Williams?
+SELECT p.ProductName
+FROM Product p
+JOIN OrderItem oi ON p.ProductID = oi.ProductID
+JOIN "Order" o ON oi.OrderID = o.OrderID
+JOIN Customer c ON o.CustomerID = c.CustomerID
+WHERE c.FirstName = 'Sophia' AND c.LastName = 'Williams';
+
+--Retrieve all orders with a total amount greater than $600.
+SELECT *
+FROM "Order"
+WHERE TotalAmount > 600;
+
+--List the products with a price lower than $25.
+SELECT ProductName
+FROM Product
+WHERE Price < 25;
+
+--Find the email address of the customer who made the order with the highest total amount.
+SELECT c.Email
+FROM Customer c
+JOIN "Order" o ON c.CustomerID = o.CustomerID
+WHERE o.TotalAmount = (SELECT MAX(TotalAmount) FROM "Order");
+
+--Which customers placed orders in January 2023?
+SELECT DISTINCT c.FirstName, c.LastName
+FROM Customer c
+JOIN "Order" o ON c.CustomerID = o.CustomerID
+WHERE o.OrderDate BETWEEN '2023-01-01' AND '2023-01-31';
+
+--Retrieve the order date and total amount for orders placed by customers who joined in 2021.
+
+--What products were ordered by customer Ava Moore in her order placed on September 10, 2022?
+
+--List the order IDs and customer IDs for orders with a total amount between $400 and $500.
+SELECT OrderID, CustomerID
+FROM "Order"
+WHERE TotalAmount BETWEEN 400 AND 500;
