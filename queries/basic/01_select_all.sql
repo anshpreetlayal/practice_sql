8794ee1486df676a39a56ebe7f6ae88dfc802213
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-05 12:21:43 -0500

    practicing select queries

--- queries/basic/01_select_all.sql
+++ queries/basic/01_select_all.sql
@@ -1,65 +1,46 @@
 -- I will be using this website to practice - https://sqliteonline.com/
-Select * from demo;
 -- Retries customer id from the customer table
-Select CustomerID from Customer;
 
 -- Retrieve all customer data from the Customer table.
-Select * from Customer;
 
 -- Get the first and last names of customers who joined on a specific DateJoined.
- Select FirstName, LastName
-  from Customer
-  WHERE DateJoined = "2020-09-19";
+
 
 -- Fetch the email and phone number of a customer with a specific CustomerID.
-Select email, phone
-from customer
-where CustomerID = "12";
+
 
 --List all orders from the Order table.
-select * from Order;
+
 
 -- Retrieve the total amount of a specific order using its OrderID.
-Select order
+
 
 -- Get the OrderDate of a particular order.
-SELECT OrderDate
-FROM Order
-WHERE OrderID = 12;
+
 
 --  List all order items from the OrderItem table.
-SELECT *
-FROM OrderItem;
+
 
 
 --  Retrieve the quantity of a specific product in an order item.
 
 
 -- List all products from the Product table.
-select * from Product;
+s
 
 -- Get the name and price of a specific product.
-SELECT ProductName, Price
-FROM Product
-WHERE ProductID = 104;
+
 
 --Retrieve all customer data from the Customer table.
-SELECT *
-FROM Customer;
+
 
 --Get the first and last names of customers who joined on a specific DateJoined.
-Select FirstName, LastName
-from Customer
-where DateJoined = " 2020-08-12";
 
 --Fetch the email and phone number of a customer with a specific CustomerID.
-SELECT Email, Phone
-FROM Customer
-WHERE CustomerID = 12;
+
 
 --  List all orders from the Order table.
-SELECT *
-FROM Order;
+
 
  -- Retrieve the total amount of a specific order using its OrderID.
  SELECT TotalAmount
