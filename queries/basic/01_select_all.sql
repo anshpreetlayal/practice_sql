fc71ebcb79baa38017392ae392c93cbb869fca6f
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-06 13:40:51 -0500

    practicing select queries

--- queries/basic/01_select_all.sql
+++ queries/basic/01_select_all.sql
@@ -1,74 +1,40 @@
-
 -- 1. Select all first names from the Customer table.
 
 -- 2. Select all last names from the Customer table.
 
 -- 3. Select all order dates from the "Order" table.
 
-
-
 -- 4. Select all product names from the "Product" table.
-
-
 
 -- 5. Select all email addresses of customers who joined in 2021.
 
-
-
 -- 6.Select all total order amounts from the "Order" table.
-
-
 
 -- 7.Select all quantities of products in the "OrderItem" table.
 
-
-
 -- 8. Select all first names and phone numbers of customers.
-
-
 
 -- 9.Select all total order amounts for orders with an amount greater than 700.
 
-
-
 --  10. Select all product names and prices from the "Product" table.
-
 
 --  11. Select all last names of customers who joined before 2020.
 
-
-
 -- 12. Select all order IDs and customer IDs from the "Order" table.
-
-
 
 -- 13.Select all order dates and total order amounts from the "Order" table.
 
-
-
 -- 14. Select all product IDs from the "OrderItem" table.
-
-
 
 --  15. Select all first names of customers who joined in 2022.
 
-
-
 -- 16. Select all product names and quantities from the "Product" and "OrderItem" tables.
-
-
 
 -- 17. Select all email addresses and phone numbers of customers.
 
-
-
 -- 18. Select all order IDs and total order amounts for orders placed in January 2023.
 
-
-
 --  19. Select all product names and their prices from the "Product" table.
-
-
 
 -- 20.  Select all first names and last names of customers who joined before September 2020.
 
