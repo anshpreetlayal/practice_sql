94dd5124a28cdcc3fc49536808b1479e0f93bf0d
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-06 11:36:37 -0500

    practicing select queries

--- queries/basic/02_select_columns.sql
+++ queries/basic/02_select_columns.sql
@@ -1,54 +1,33 @@
---Practice questions
 
---Select the first and last names of all customers.
+--1. Select the first and last names of all customers.
 
+--2. Select the order dates from the "Order" table.
 
---Select the order dates from the "Order" table.
+--3. Select the product names from the "Product" table.
 
+--4. Select the email addresses of customers who joined in 2021.
 
---Select the product names from the "Product" table.
+--5. Select the total order amounts from the "Order" table.
 
+--6. Select the quantity of products in the "OrderItem" table.
 
+--7. Select the first name and phone number of customers.
 
---Select the email addresses of customers who joined in 2021.
+--8. Select the total order amounts for orders with an amount greater than 700.
 
+--9. Select the product names and prices from the "Product" table.
 
+--10. Select the last names of customers who joined before 2020.
 
---Select the total order amounts from the "Order" table.
+--11. Select the order IDs and customer IDs from the "Order" table.
 
+--12. Select the order dates and total order amounts from the "Order" table.
 
+--13. Select the product IDs from the "OrderItem" table.
 
---Select the quantity of products in the "OrderItem" table.
+--14. Select the first names of customers who joined in 2022.
 
-
-
---Select the first name and phone number of customers.
-
-
-
---Select the total order amounts for orders with an amount greater than 700.
-
-
-
---Select the product names and prices from the "Product" table.
-
-
---Select the last names of customers who joined before 2020.
-
-
---Select the order IDs and customer IDs from the "Order" table.
-
-
---Select the order dates and total order amounts from the "Order" table.
-
-
---Select the product IDs from the "OrderItem" table.
-
-
---Select the first names of customers who joined in 2022.
-
-
---Select the product names and quantities from the "Product" and "OrderItem" tables.
+--15. Select the product names and quantities from the "Product" and "OrderItem" tables.
 
 
 
