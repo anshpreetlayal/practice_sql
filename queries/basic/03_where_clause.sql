c8dddb470cbf35c52e0c412256b08fe2cd99e0e1
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-06 13:03:41 -0500

    practicing select queries

--- queries/basic/03_where_clause.sql
+++ queries/basic/03_where_clause.sql
@@ -18,7 +18,25 @@
 
 --10. List the order IDs and customer IDs for orders with a total amount between $400 and $500.
 
+--11. Find the total quantity of products ordered by customer James Davis.
 
+--12. Which customers joined in 2019 or later?
+
+--13. What products were ordered by customer Emily Johnson in orders with a total amount greater than $500?
+
+--14.  Retrieve all orders with a total amount between $300 and $400.
+
+--15. List the products with a price between $20 and $30.
+
+--16. Find the email address of the customer who made the earliest order.
+
+--17.Which customers placed orders in February 2022?
+
+--18. Retrieve the order date and total order amounts for orders placed by customers who joined in 2020 or earlier.
+
+--19. What products were ordered by customer Noah Martin in orders placed on or after September 2022?
+
+--20. List the order IDs and customer IDs for orders with a total amount greater than $800.
 
 
 
