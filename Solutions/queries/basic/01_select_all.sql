d8a66a748a9076c8f67a12abb51c1f124b608ce3
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-06 21:12:15 -0500

    practicing select queries

--- Solutions/queries/basic/01_select_all.sql
+++ Solutions/queries/basic/01_select_all.sql
@@ -15,7 +15,8 @@ SELECT ProductName
 FROM Product;
 
 -- 5. Select all email addresses of customers who joined in 2021.
-SELECT
+SELECT Email
+FROM
 -- 6.Select all total order amounts from the "Order" table.
 
 -- 7.Select all quantities of products in the "OrderItem" table.
