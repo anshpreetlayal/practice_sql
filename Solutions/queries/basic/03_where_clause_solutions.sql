585166dfe42c59d5e470d8f36ce98e1f2aaa6a7b
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-14 20:06:31 -0500

    practicing  queries

--- Solutions/queries/basic/03_where_clause_solutions.sql
+++ Solutions/queries/basic/03_where_clause_solutions.sql
@@ -56,3 +56,5 @@ WHERE TotalAmount BETWEEN 400 AND 500;
 
 ---14. Retrieve orders placed by customers with names starting with 'J'.
 
+---15. Fetch orders made by a specific CustomerID and with TotalAmount over $700.
+
