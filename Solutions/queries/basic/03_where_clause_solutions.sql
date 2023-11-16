6ef3d4fdc98f4fa126009dde2dc3d04c6febb26d
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-15 19:52:54 -0500

    practicing  queries

--- Solutions/queries/basic/03_where_clause_solutions.sql
+++ Solutions/queries/basic/03_where_clause_solutions.sql
@@ -57,3 +57,4 @@ WHERE TotalAmount BETWEEN 400 AND 500;
 --14.  Retrieve all orders with a total amount between $300 and $400.
 --15. List the products with a price between $20 and $30.
 --16. Find the email address of the customer who made the earliest order.
+--17.Which customers placed orders in February 2022?
