f2ff9de9ff69b905f9d60dbd7fac727c90cb031a
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-22 15:38:49 -0500

    practicing  queries

---
+++ Intermediate/02_join_tables.sql
@@ -0,0 +1,20 @@
+Retrieve orders with customer information.
+Get order details along with product information.
+Fetch customer information for orders placed.
+Get product details for each order item.
+Retrieve orders with customer names and order dates.
+Get product names along with their prices for each order item.
+Fetch customer information for orders with total amounts.
+Get product details along with order quantities.
+Retrieve orders with customer email addresses.
+Get product names and prices for each order item.
+Fetch customer names and order dates for orders placed.
+Get order details along with product names and quantities.
+Retrieve orders with customer names and order total amounts.
+Get product names and prices for each order item.
+Fetch customer names and order dates for orders placed in 2023.
+Get order details along with product names and quantities for orders placed.
+Retrieve orders with customer names and total order amounts placed in 2022.
+Get product names and prices for each order item placed in 2021.
+Fetch customer names and order dates for orders placed in 2020.
+Get order details along with product names and quantities for orders placed in 2019.
--- Solutions/queries/Intermediate/01_group_by_solutions.sql
+++ Solutions/queries/Intermediate/01_group_by_solutions.sql
@@ -110,5 +110,7 @@ WHERE YEAR(OrderDate) = 2020
 GROUP BY OrderYear, OrderMonth;
 
 ---19. Get the count of orders placed on each day of the week.
+
+
 ---20. Find the total number of products in different categories.
 
