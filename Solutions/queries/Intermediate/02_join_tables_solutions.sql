5460cdae440084d02d8fdbfd16f77460ee102298
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-22 19:22:46 -0500

    practicing  queries

---
+++ Solutions/queries/Intermediate/02_join_tables_solutions.sql
@@ -0,0 +1,39 @@
+--1. Retrieve orders with customer information.
+
+--2. Get order details along with product information.
+
+--3.Fetch customer information for orders placed.
+
+--4. Get product details for each order item.
+
+--5. Retrieve orders with customer names and order dates.
+
+--6. Get product names along with their prices for each order item.
+
+--7. Fetch customer information for orders with total amounts.
+
+--8. Get product details along with order quantities.
+
+--9. Retrieve orders with customer email addresses.
+
+--10. Get product names and prices for each order item.
+
+--11. Fetch customer names and order dates for orders placed.
+
+--12. Get order details along with product names and quantities.
+
+--13. Retrieve orders with customer names and order total amounts.
+
+--14. Get product names and prices for each order item.
+
+--15. Fetch customer names and order dates for orders placed in 2023.
+
+--16. Get order details along with product names and quantities for orders placed.
+
+--17. Retrieve orders with customer names and total order amounts placed in 2022.
+
+--18.Get product names and prices for each order item placed in 2021.
+
+--19.Fetch customer names and order dates for orders placed in 2020.
+
+--20. Get order details along with product names and quantities for orders placed in 2019.
