c1375cdcda5891cf0a0a77081704afedb8a660e7
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-22 19:21:46 -0500

    practicing  queries

---
+++ Solutions/queries/Intermediate/03_subqueries_solutions.sql
@@ -0,0 +1,39 @@
+---1. Retrieve the average total amount of orders.
+
+---2. Get the customer details who placed orders with amounts greater than the average.
+
+---3. Find the product names with prices higher than the average price.
+
+---4. Retrieve customers who joined after the earliest join date.
+
+---5. Get orders with amounts greater than the maximum order amount.
+
+---6. Find the product names with prices lower than the minimum price.
+
+---7. Retrieve customers who have placed orders.
+
+---8. Get orders placed by customers with IDs greater than 10.
+
+---9. Find customers who joined after a specific customer's join date.
+
+---10. Retrieve orders with amounts less than the average order amount.
+
+---11. Get orders placed by customers with specific email domains.
+
+---12. Retrieve customers who have not placed any orders.
+
+---13. Get orders placed before a specific order date.
+
+---14. Find customers who joined before the average join date.
+
+---15. Retrieve orders with amounts greater than the sum of all order amounts divided by the count of orders.
+
+---16. Get orders placed by customers with names starting with a specific letter.
+
+---17. Find customers who joined before any customer with a specific customer ID.
+
+---18. Retrieve orders with amounts less than the maximum order amount divided by 2.
+
+---19. Get products with prices higher than the average product price.
+
+---20. Find customers who joined after a specific date.
