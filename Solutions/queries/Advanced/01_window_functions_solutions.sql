c4fb9bc73d687a16b55ce85ceaebb48df2825abf
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-24 19:29:43 -0500

    practicing  queries

--- Solutions/queries/Advanced/01_window_functions_solutions.sql
+++ Solutions/queries/Advanced/01_window_functions_solutions.sql
@@ -1,3 +1,4 @@
 ---1. What SQL query would rank products based on their prices using the ROW_NUMBER window function?
 ---2. How can you calculate the running total of sales amount for each order using the SUM window function?
 ---3. Retrieve the average price of products within their respective categories using the AVG window function.
+---4. Get the row number of customers ordered by their joining dates using the ROW_NUMBER window function.
