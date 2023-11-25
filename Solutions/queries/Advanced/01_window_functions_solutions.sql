37e895c6aa4ecbfbf83bae53745e4115801aaddd
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-24 19:31:36 -0500

    practicing  queries

--- Solutions/queries/Advanced/01_window_functions_solutions.sql
+++ Solutions/queries/Advanced/01_window_functions_solutions.sql
@@ -9,3 +9,7 @@
 ---9. Get customer information for orders with total amounts using the PARTITION BY clause with the SUM window function.
 ---10. Retrieve product names and prices for each order item using the PARTITION BY clause with the ROW_NUMBER window function.
 ---11. Fetch customer names and order dates for orders placed in 2023 using the PARTITION BY clause with the FILTER/WHERE condition.
+---12. Get order details along with product names and quantities using the PARTITION BY clause with the SUM window function.
+---13. Retrieve orders with customer names and order total amounts using the PARTITION BY clause with the SUM window function.
+---14. Fetch product names and prices for each order item using the PARTITION BY clause with the ROW_NUMBER window function.
+---15. Get customer names and order dates for orders placed in 2022 using the PARTITION BY clause with the FILTER/WHERE condition.
