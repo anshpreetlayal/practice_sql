39c658405262a6ca5336529f9f4d2c530301532c
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-24 19:29:22 -0500

    practicing  queries

--- Solutions/queries/Advanced/01_window_functions_solutions.sql
+++ Solutions/queries/Advanced/01_window_functions_solutions.sql
@@ -1,1 +1,2 @@
 ---1. What SQL query would rank products based on their prices using the ROW_NUMBER window function?
+---2. How can you calculate the running total of sales amount for each order using the SUM window function?
