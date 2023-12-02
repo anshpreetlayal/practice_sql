8ff08d01013062113763aa420d97de6ee5a3638a
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-12-02 18:27:20 -0500

    practicing  queries

--- Solutions/queries/Advanced/01_window_functions_solutions.sql
+++ Solutions/queries/Advanced/01_window_functions_solutions.sql
@@ -21,6 +21,10 @@ FROM Customer;
 ---6. Retrieve the cumulative sum of product quantities ordered using the SUM window function.
 SELECT SUM ()
 ---7. Can you rank customers based on their total order amounts using the RANK window function?
+SELECT
+    CustomerID,
+    RANK() OVER (ORDER BY TotalOrderAmount DESC) AS CustomerRank
+
 
 ---8. How to fetch product details along with order quantities using the PARTITION BY clause with the SUM window function?
 
