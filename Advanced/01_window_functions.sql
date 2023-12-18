c22cb0f2b2c8b482147ec473011912adafacd4bb
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-12-18 18:57:03 -0500

    practicing  queries

--- Advanced/01_window_functions.sql
+++ Advanced/01_window_functions.sql
@@ -1,32 +1,18 @@
 ---1. What SQL query would rank products based on their prices using the ROW_NUMBER window function?
-SELECT ProductID, ProductName, Price,
-       ROW_NUMBER() OVER (ORDER BY Price) AS PriceRank
-FROM Product;
 
 ---2. How can you calculate the running total of sales amount for each order using the SUM window function?
-SELECT OrderID, TotalAmount,
-       SUM(TotalAmount) OVER (ORDER BY OrderDate) AS RunningTotal
-FROM "Order";
 
 ---3. Retrieve the average price of products within their respective categories using the AVG window function.
-SELECT ProductID, ProductName, Price,
-       AVG(Price) OVER (PARTITION BY ProductID) AS AvgPricePerCategory
-FROM Product;
+
 
 ---4. Get the row number of customers ordered by their joining dates using the ROW_NUMBER window function.
-SELECT CustomerID, FirstName, LastName, DateJoined,
-       ROW_NUMBER() OVER (ORDER BY DateJoined) AS JoiningDateRank
-FROM Customer;
+
 
 ---5. What query can display the total sales amount for each year using the SUM window function?
-SELECT OrderID, TotalAmount,
-       SUM(TotalAmount) OVER (PARTITION BY YEAR(OrderDate)) AS TotalSalesPerYear
-FROM "Order";
+
 
 ---6. Retrieve the cumulative sum of product quantities ordered using the SUM window function.
-SELECT OrderItemID, OrderID, ProductID, Quantity,
-       SUM(Quantity) OVER (ORDER BY OrderID) AS CumulativeQuantity
-FROM OrderItem;
+
 
 ---7. Can you rank customers based on their total order amounts using the RANK window function?
 
---
+++ Advanced/02_cte.sql
@@ -0,0 +1,8 @@
+
+--1. Retrieve the total number of orders placed by each customer.
+
+--2. Calculate the total sales amount for each year.
+
+--3. Find the average price of products within their respective categories.
+
+--4. Assign row numbers to customers ordered by their joining dates.
