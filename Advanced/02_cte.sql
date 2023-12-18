adaf194f858553187add3a12ca30020cd777b303
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-12-18 18:58:06 -0500

    practicing  queries

--- Advanced/02_cte.sql
+++ Advanced/02_cte.sql
@@ -6,3 +6,15 @@
 --3. Find the average price of products within their respective categories.
 
 --4. Assign row numbers to customers ordered by their joining dates.
+
+--5. Get the cumulative sum of product quantities ordered.
+
+--6. Rank customers based on their total order amounts.
+
+--7. Fetch product details along with order quantities.
+
+--8. Retrieve customer information for orders with total amounts.
+
+--9. Fetch product names and prices for each order item.
+
+--10. Determine the count of orders placed by each customer in a specific month.
