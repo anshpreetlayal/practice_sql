d4d8c536d519fb4d48eac9d9deeb4472fb938432
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-12-18 19:03:40 -0500

    practicing  queries

--- Advanced/03_indexing.sql
+++ Advanced/03_indexing.sql
@@ -1,3 +1,9 @@
 --1. How would you create an index on the 'ProductName' column of the 'Product' table?
 
 --2. Explain the significance of indexing in a database and its impact on query performance.
+
+--3. What types of indexes can be created in SQL? Provide examples for each.
+
+--4. Describe the process of dropping an index from a table in SQL.
+
+--5. How do you add a composite index involving multiple columns in a table?
