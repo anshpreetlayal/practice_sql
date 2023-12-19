7692789ad523855c588ca2199cd7dadbe84ab987
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-12-18 19:04:36 -0500

    practicing  queries

--- Advanced/03_indexing.sql
+++ Advanced/03_indexing.sql
@@ -13,3 +13,9 @@
 --7. How does indexing affect INSERT, UPDATE, and DELETE operations on a table?
 
 --8. Illustrate how you'd check if an index exists on a particular table and column in SQL.
+
+--9. What are the key factors to consider before creating an index on a column?
+
+--10. Explain the impact of indexing on sorting and searching operations in SQL.
+
+--11. How do you identify unused or redundant indexes in a database?
