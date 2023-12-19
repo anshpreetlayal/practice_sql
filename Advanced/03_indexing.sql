137bf3438342e1700b555c6777ba70265c65c066
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-12-18 19:03:20 -0500

    practicing  queries

---
+++ Advanced/03_indexing.sql
@@ -0,0 +1,3 @@
+--1. How would you create an index on the 'ProductName' column of the 'Product' table?
+
+--2. Explain the significance of indexing in a database and its impact on query performance.
